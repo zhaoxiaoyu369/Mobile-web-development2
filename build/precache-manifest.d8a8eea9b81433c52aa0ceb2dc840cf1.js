2b124eef1199f8b7831749570cc0db07096bca34
zhaoxiaoyu369 <995271283@qq.com>
2019-11-07 20:46:22 +0800

    work2
    work2

---
+++ build/precache-manifest.d8a8eea9b81433c52aa0ceb2dc840cf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952167cc811cfa188ab1ba6c2d1c6883",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cbdc093c9f5ef582e15",
+    "url": "/static/css/main.4c73cbf8.chunk.css"
+  },
+  {
+    "revision": "6e98f0c4e6b2a6803da4",
+    "url": "/static/js/2.c682078b.chunk.js"
+  },
+  {
+    "revision": "8cbdc093c9f5ef582e15",
+    "url": "/static/js/main.9148a958.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
